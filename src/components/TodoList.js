44efd5988dc0587b9bb3b4111361cb776cf1fd99
jolene <sitienijolene@gmail.com>
2024-06-04 02:34:20 +0300

    write the code that renders a list of todos as a list of read-only input

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,1 +1,26 @@
 //component will be responsible of displaying the todo list items on the screen
+import React from 'react';
+
+//'todos' is an array of todo obj and 'setTodos' is a function to update the 'todos state'
+const TodoList = ({ todos, setTodos }) => {
+    return(
+        <div>
+            {/* to iterate through the entire 'todo' array and return the jsx below */}
+            {todos.map((todo) => {
+                //the todo.id is a unique identifier for each li
+                <li className='todo-list' key = {todo.id}>
+                    <input
+                    type='text'
+                    //displays title of current todo
+                    value={todo.title}
+                    className='list'
+                    // this prop is set to an arrow function that prevents the defau;t behaviour of the inpit field when its value changes(prevents input field from being editable)
+                    onChange={(e) => e.preventDefault()}
+                    />
+                </li>
+            })}
+        </div>
+    );
+}
+
+export default TodoList;
