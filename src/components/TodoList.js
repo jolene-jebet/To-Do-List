468c68e2f5debd1a690ade43b474c50a3d155e41
jolene <sitienijolene@gmail.com>
2024-06-04 09:34:59 +0300

    create a function that handles whether an item in the todo is completed or not

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -9,6 +9,26 @@ const TodoList = ({ todos, setTodos }) => {
         setTodos(todos.filter((todo) => todo.id !== id ));
     }
 
+    //function that checks out an item from the to-do list
+    const handleComplete = (todo) => {
+        //this function is called to update the state of the todos array
+        setTodos(
+            //used to iterate over each item in the todos and teturns a new array with the update todo items
+            todos.map((item) => {
+                //if the id of current item matches the id of the todo passed as a parameter
+                if (item.id === todo.id){
+                    return{
+                        //used to create a copy of the item object
+                        ...item,
+                        //its toggled by setting it to the opposite of its current value. this effectively marks the todo item as completed or uncompleted
+                        completed: !item.completed}
+                }
+                //if id of current item doesnt match the id of the todo passed as a parameter, the item is returned as it is without any modifications
+                return item;
+            })
+        );
+    }
+
     return(
         <div>
             {/* to iterate through the entire 'todo' array and return the jsx below */}
@@ -25,7 +45,7 @@ const TodoList = ({ todos, setTodos }) => {
                     />
                     {/* div containing the buttons for edit, delete and complete */}
                     <div>
-                        <button className='button-complete task-button'>
+                        <button className='button-complete task-button' onClick = { () => handleComplete(todo) }>
                             <i className='fa fa-check-circle'></i>
                         </button>
                         <button className='button-edit task-button'>
