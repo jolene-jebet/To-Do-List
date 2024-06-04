2fe910c2938f195479813cb091c09f62c70b694c
jolene <sitienijolene@gmail.com>
2024-06-04 03:01:49 +0300

    create the edit, check and delete button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,9 +6,9 @@ const TodoList = ({ todos, setTodos }) => {
     return(
         <div>
             {/* to iterate through the entire 'todo' array and return the jsx below */}
-            {todos.map((todo) => {
+            {todos.map((todo) => (
                 //the todo.id is a unique identifier for each li
-                <li className='todo-list' key = {todo.id}>
+                <li className='list-item' key = {todo.id}>
                     <input
                     type='text'
                     //displays title of current todo
@@ -17,8 +17,20 @@ const TodoList = ({ todos, setTodos }) => {
                     // this prop is set to an arrow function that prevents the defau;t behaviour of the inpit field when its value changes(prevents input field from being editable)
                     onChange={(e) => e.preventDefault()}
                     />
+                    {/* div containing the buttons for edit, delete and complete */}
+                    <div>
+                        <button className='button-complete task-button'>
+                            <i className='fa fa-check-circle'></i>
+                        </button>
+                        <button className='button-edit task-button'>
+                            <i className='fa fa-edit'></i>
+                        </button>
+                        <button className='button-delete task-button'>
+                            <i className='fa fa-trash'></i>
+                        </button>
+                    </div>
                 </li>
-            })}
+            ))}
         </div>
     );
 }
