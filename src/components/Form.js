43cce55ee505ab927eb1fabe922721e787a051a9
jolene <sitienijolene@gmail.com>
2024-06-15 11:40:03 +0300

    update the form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 // we need an unique id for each todo item hence we will use the u uid
 import { v4 as uuidv4 } from 'uuid';
 
 
-const Form = ({ input, setInput, todos, setTodos }) => {
+const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
+    //unpates an excisting todo item
+    const updateTodo = (title, id, completed) => {
+        //maps through the entire array to check if the ids match if they do, it returns a new object w the updated title and completed values else returns original todo object 
+        const newTodo = todos.map((todo) => 
+            todos.id === id ? { title, id, completed } : todo
+        )
+        //updates the todos state with the newTode array
+        setTodos(newTodo);
+        //resets the editTodo state to an empty string 
+        setEditTodo('');
+    }
+
     //handles the input change event by updating its state with current value of input field using setInput function
     const onInputChange = (e) => {
         //update input state w current value of input field
@@ -13,18 +25,27 @@ const Form = ({ input, setInput, todos, setTodos }) => {
     const onFormSubmit = (e) => {
         //prevents default form submission behaviour
         e.preventDefault();
-        //updates the todos state by adding a new todo object
-        setTodos([
-            //spreads existing todos array 
-            ...todos,
-            //adds new todo obj with unique id current input value as the title, and completed status as false
-            {id: uuidv4(),
-                //current input value as the title
-                title: input,
-                    //completed status as false 
-                    completed: false }]);
-        //clears the input field by setting it to an empty string            
-        setInput('');
+        //if editTodo is falsy(not editing an existing todo )
+        if(!editTodo){
+            ///it creates a new todo obj with a unique id w the current input value as the title and complete set to false
+            //updates the todos state by adding a new todo object
+            setTodos([
+                //spreads existing todos array 
+                ...todos,
+                //adds new todo obj with unique id current input value as the title, and completed status as false
+                {id: uuidv4(),
+                    //current input value as the title
+                    title: input,
+                        //completed status as false 
+                        completed: false }]);
+            //clears the input field by setting it to an empty string            
+            setInput('');            
+        }else{
+            //if its truthy(editing an existing todo) it calls the updateTodo function w the id of the todo being edited and its completed status
+            updateTodo( input, editTodo.id, editTodo.completed)
+        }
+        
+        
     }
 
     //when the form is submitted it triggers the onFormSubmit function
